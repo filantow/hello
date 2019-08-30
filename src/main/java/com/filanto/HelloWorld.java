5760d31667ee0dbb22f1e7e0b98a4689c6df021a
filanto <filanto1110@gmail.com>
2019-08-30 16:51:23 +0800

    add print class

--- src/main/java/com/filanto/HelloWorld.java
+++ src/main/java/com/filanto/HelloWorld.java
@@ -3,5 +3,6 @@ package com.filanto;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        new Print().main("hello world 2");
     }
 }
---
+++ src/main/java/com/filanto/Print.java
@@ -0,0 +1,8 @@
+package com.filanto;
+
+public class Print {
+    public static void main(String args) {
+        System.out.println(args);
+    }
+
+}
